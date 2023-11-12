3aeebfded1e26ef59c6a456a8602258defdaafe8
Deathwing <deathwing_shadow@hotmail.de>
2023-11-12 10:52:06 +0100

    Initial commit

---
+++ Assets/RlyNetwork/Runtime/Scripts/IUnitySdkPlatform.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+
+public interface IUnitySdkPlatform
+{
+    Task<string> GetBundleId();
+    Task<string> GetMnemonic();
+    Task<string> GenerateNewMnemonic();
+    Task<bool> SaveMnemonic(string mnemonic, bool saveToCloud, bool rejectOnCloudSaveFailure);
+    Task<bool> DeleteMnemonic();
+    Task<string> GetPrivateKeyFromMnemonic(string mnemonic);
+}
